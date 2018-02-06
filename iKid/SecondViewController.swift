e029b35c64829fac2c1d4db219076d1daef25c8d
Connor Hawthorne <con4man@github.com>
2018-02-06 14:02:12 -0800

    Initial Commit

---
+++ iKid/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  iKid
+//
+//  Created by Connor Hawthorne on 2/6/18.
+//  Copyright © 2018 Connor Hawthorne. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
